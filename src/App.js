3b6a989360f98dbe2c394814b610762c8ffe2804
mi8722663079 <mi8722663@gmail.com>
2023-10-08 08:01:18 +0200

    let goooo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const Person = () => {
+  const sum = 4 + 44;
+  return (
+    <>
+      <h1>the sum is</h1>
+      <h1>{sum}</h1>
+    </>
+  );
+};
+const App = () => {
+  const name = ["mohamed ", " ali", " yousef"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, {name[0]}!</h1>
+      <Person />
+      {name[0].includes("mohamed") ? (
+        <>
+          <h1>there's a name</h1>
+          <h2>it's called mohamed</h2>
+        </>
+      ) : (
+        <>test</>
+      )}
     </div>
   );
-}
+};
 
 export default App;
